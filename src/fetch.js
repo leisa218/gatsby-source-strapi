d793d1c1146dc9e9c0bf0ed08d3f7681682b0ce7
Natalie Klinke <nk@10vorne.it>
2019-07-16 11:49:01 +0300

    apply changes to add language parameters to fiels with the naming convention fieldname_lang and make this fields accssible by graphQL

--- src/fetch.js
+++ src/fetch.js
@@ -1,31 +1,105 @@
-import axios from 'axios'
-import { isObject, startsWith, forEach } from 'lodash'
-import pluralize from 'pluralize'
+'use strict';
+var _defineProperty2 = require('babel-runtime/helpers/defineProperty');
 
-module.exports = async ({ apiURL, contentType, jwtToken, queryLimit }) => {
-  console.time('Fetch Strapi data')
-  console.log(`Starting to fetch data from Strapi (${pluralize(contentType)})`)
+var _defineProperty3 = _interopRequireDefault(_defineProperty2);
 
-  // Define API endpoint.
-  const apiEndpoint = `${apiURL}/${pluralize(contentType)}?_limit=${queryLimit}`
+var _extends3 = require('babel-runtime/helpers/extends');
 
-  // Set authorization token
-  let fetchRequestConfig = {}
-  if (jwtToken !== null) {
-    fetchRequestConfig.headers = {
-      Authorization: `Bearer ${jwtToken}`,
-    }
-  }
+var _extends4 = _interopRequireDefault(_extends3);
 
-  // Make API request.
-  const documents = await axios(apiEndpoint, fetchRequestConfig)
+var _stringify = require('babel-runtime/core-js/json/stringify');
 
-  // Query all documents from client.
-  console.timeEnd('Fetch Strapi data')
+var _stringify2 = _interopRequireDefault(_stringify);
 
-  // Map and clean data.
-  return documents.data.map(item => clean(item))
-}
+// ---original
+
+var _regenerator = require('babel-runtime/regenerator');
+
+var _regenerator2 = _interopRequireDefault(_regenerator);
+
+var _asyncToGenerator2 = require('babel-runtime/helpers/asyncToGenerator');
+
+var _asyncToGenerator3 = _interopRequireDefault(_asyncToGenerator2);
+
+var _axios = require('axios');
+
+var _axios2 = _interopRequireDefault(_axios);
+
+var _lodash = require('lodash');
+
+var _pluralize = require('pluralize');
+
+var _pluralize2 = _interopRequireDefault(_pluralize);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+module.exports = function () {
+  var _ref = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee(_ref2) {
+    var apiURL = _ref2.apiURL,
+        contentType = _ref2.contentType,
+        jwtToken = _ref2.jwtToken,
+        availableLngs = _ref2.availableLngs,
+        queryLimit = _ref2.queryLimit;
+    var apiEndpoint, fetchRequestConfig, documents;
+    return _regenerator2.default.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            console.time('Fetch Strapi data');
+            console.log('Starting to fetch data from Strapi (' + (0, _pluralize2.default)(contentType) + ')');
+
+            // Define API endpoint.
+            apiEndpoint = apiURL + '/' + (0, _pluralize2.default)(contentType) + '?_limit=' + queryLimit;
+
+            // Set authorization token
+
+            fetchRequestConfig = {};
+
+            if (jwtToken !== null) {
+              fetchRequestConfig.headers = {
+                Authorization: 'Bearer ' + jwtToken
+              };
+            }
+
+            // Make API request.
+            _context.next = 7;
+            return (0, _axios2.default)(apiEndpoint, fetchRequestConfig);
+
+          case 7:
+            documents = _context.sent;
+
+
+            // Query all documents from client.
+            console.timeEnd('Fetch Strapi data');
+
+            // Map and clean data.
+            return _context.abrupt('return', documents.data.map(function (item) {
+              var cleanItem = clean(item);
+
+              if (availableLngs.length) {
+                cleanItem.locales = availableLngs.map(function (lng) {
+                  return {
+                    lng: lng
+                  };
+                });
+
+                return localize(cleanItem);
+              }
+              return clean(item);
+            }));
+
+          case 10:
+          case 'end':
+            return _context.stop();
+        }
+      }
+    }, _callee, undefined);
+  }));
+
+  return function (_x) {
+    return _ref.apply(this, arguments);
+  };
+}();
 
 /**
  * Remove fields starting with `_` symbol.
@@ -33,17 +107,41 @@ module.exports = async ({ apiURL, contentType, jwtToken, queryLimit }) => {
  * @param {object} item - Entry needing clean
  * @returns {object} output - Object cleaned
  */
-const clean = item => {
-  forEach(item, (value, key) => {
-    if (startsWith(key, `__`)) {
-      delete item[key]
-    } else if (startsWith(key, `_`)) {
-      delete item[key]
-      item[key.slice(1)] = value
-    } else if (isObject(value)) {
-      item[key] = clean(value)
+var clean = function clean(item) {
+  (0, _lodash.forEach)(item, function (value, key) {
+    if ((0, _lodash.startsWith)(key, '__')) {
+      delete item[key];
+    } else if ((0, _lodash.startsWith)(key, '_')) {
+      delete item[key];
+      item[key.slice(1)] = value;
+    } else if ((0, _lodash.isObject)(value)) {
+      item[key] = clean(value);
     }
-  })
+    if (key === 'data') {
+      item.data = (0, _stringify2.default)(value);
+    }
+  });
 
-  return item
-}
+  return item;
+};
+
+var localize = function localize(item) {
+  (0, _lodash.forEach)(item, function (value, key) {
+    var fieldSplit = key.split('__');
+    var fieldName = fieldSplit[0];
+    var fieldLocale = fieldSplit[1];
+
+    if (item.locales && item.locales.length && fieldLocale) {
+      item.locales = item.locales.map(function (locale) {
+        if (fieldLocale === locale.lng) {
+          return (0, _extends4.default)({}, locale, (0, _defineProperty3.default)({}, fieldName, value));
+        }
+
+        return locale;
+      });
+
+      delete item[key];
+    }
+  });
+  return item;
+};
--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,76 +1,149 @@
-import axios from 'axios'
-import fetchData from './fetch'
-import { Node } from './nodes'
-import { capitalize } from 'lodash'
-import normalize from './normalize'
+'use strict';
 
-exports.sourceNodes = async (
-  { store, boundActionCreators, cache },
-  {
-    apiURL = 'http://localhost:1337',
-    contentTypes = [],
-    loginData = {},
-    queryLimit = 100,
-  }
-) => {
-  const { createNode, touchNode } = boundActionCreators
-  let jwtToken = null
+var _regenerator = require('babel-runtime/regenerator');
 
-  // Check if loginData is set.
-  if (
-    loginData.hasOwnProperty('identifier') &&
-    loginData.identifier.length !== 0 &&
-    loginData.hasOwnProperty('password') &&
-    loginData.password.length !== 0
-  ) {
-    console.time('Authenticate Strapi user')
-    console.log('Authenticate Strapi user')
+var _regenerator2 = _interopRequireDefault(_regenerator);
 
-    // Define API endpoint.
-    const loginEndpoint = `${apiURL}/auth/local`
+var _promise = require('babel-runtime/core-js/promise');
 
-    // Make API request.
-    try {
-      const loginResponse = await axios.post(loginEndpoint, loginData)
+var _promise2 = _interopRequireDefault(_promise);
 
-      if (loginResponse.hasOwnProperty('data')) {
-        jwtToken = loginResponse.data.jwt
+var _asyncToGenerator2 = require('babel-runtime/helpers/asyncToGenerator');
+
+var _asyncToGenerator3 = _interopRequireDefault(_asyncToGenerator2);
+
+var _axios = require('axios');
+
+var _axios2 = _interopRequireDefault(_axios);
+
+var _fetch = require('./fetch');
+
+var _fetch2 = _interopRequireDefault(_fetch);
+
+var _nodes = require('./nodes');
+
+var _lodash = require('lodash');
+
+var _normalize = require('./normalize');
+
+var _normalize2 = _interopRequireDefault(_normalize);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.sourceNodes = function () {
+  var _ref = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee(_ref2, _ref3) {
+    var store = _ref2.store,
+        boundActionCreators = _ref2.boundActionCreators,
+        cache = _ref2.cache;
+    var _ref3$apiURL = _ref3.apiURL,
+        apiURL = _ref3$apiURL === undefined ? 'http://localhost:1337' : _ref3$apiURL,
+        _ref3$contentTypes = _ref3.contentTypes,
+        contentTypes = _ref3$contentTypes === undefined ? [] : _ref3$contentTypes,
+        _ref3$loginData = _ref3.loginData,
+        loginData = _ref3$loginData === undefined ? {} : _ref3$loginData,
+        _ref3$availableLngs = _ref3.availableLngs,
+        availableLngs = _ref3$availableLngs === undefined ? [] : _ref3$availableLngs,
+        _ref3$queryLimit = _ref3.queryLimit,
+        queryLimit = _ref3$queryLimit === undefined ? 100 : _ref3$queryLimit;
+    var createNode, touchNode, jwtToken, loginEndpoint, loginResponse, promises, entities;
+    return _regenerator2.default.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            createNode = boundActionCreators.createNode, touchNode = boundActionCreators.touchNode;
+            jwtToken = null;
+
+            // Check if loginData is set.
+
+            if (!(loginData.hasOwnProperty('identifier') && loginData.identifier.length !== 0 && loginData.hasOwnProperty('password') && loginData.password.length !== 0)) {
+              _context.next = 17;
+              break;
+            }
+
+            console.time('Authenticate Strapi user');
+            console.log('Authenticate Strapi user');
+
+            // Define API endpoint.
+            loginEndpoint = apiURL + '/auth/local';
+
+            // Make API request.
+
+            _context.prev = 6;
+            _context.next = 9;
+            return _axios2.default.post(loginEndpoint, loginData);
+
+          case 9:
+            loginResponse = _context.sent;
+
+
+            if (loginResponse.hasOwnProperty('data')) {
+              jwtToken = loginResponse.data.jwt;
+            }
+            _context.next = 16;
+            break;
+
+          case 13:
+            _context.prev = 13;
+            _context.t0 = _context['catch'](6);
+
+            console.error('Strapi authentication error: ' + _context.t0);
+
+          case 16:
+
+            console.timeEnd('Authenticate Strapi user');
+
+          case 17:
+
+            // Generate a list of promises based on the `contentTypes` option.
+            promises = contentTypes.map(function (contentType) {
+              return (0, _fetch2.default)({
+                apiURL: apiURL,
+                contentType: contentType,
+                jwtToken: jwtToken,
+                availableLngs: availableLngs,
+                queryLimit: queryLimit
+              });
+            });
+
+            // Execute the promises.
+
+            _context.next = 20;
+            return _promise2.default.all(promises);
+
+          case 20:
+            entities = _context.sent;
+            _context.next = 23;
+            return _normalize2.default.downloadMediaFiles({
+              entities: entities,
+              apiURL: apiURL,
+              store: store,
+              cache: cache,
+              createNode: createNode,
+              touchNode: touchNode,
+              jwtToken: jwtToken
+            });
+
+          case 23:
+            entities = _context.sent;
+
+
+            contentTypes.forEach(function (contentType, i) {
+              var items = entities[i];
+              items.forEach(function (item, i) {
+                var node = (0, _nodes.Node)((0, _lodash.capitalize)(contentType), item);
+                createNode(node);
+              });
+            });
+
+          case 25:
+          case 'end':
+            return _context.stop();
+        }
       }
-    } catch (e) {
-      console.error('Strapi authentication error: ' + e)
-    }
+    }, _callee, undefined, [[6, 13]]);
+  }));
 
-    console.timeEnd('Authenticate Strapi user')
-  }
-
-  // Generate a list of promises based on the `contentTypes` option.
-  const promises = contentTypes.map(contentType =>
-    fetchData({
-      apiURL,
-      contentType,
-      jwtToken,
-      queryLimit,
-    })
-  )
-
-  // Execute the promises.
-  let entities = await Promise.all(promises)
-
-  entities = await normalize.downloadMediaFiles({
-    entities,
-    apiURL,
-    store,
-    cache,
-    createNode,
-    touchNode,
-    jwtToken,
-  })
-
-  contentTypes.forEach((contentType, i) => {
-    const items = entities[i]
-    items.forEach((item, i) => {
-      const node = Node(capitalize(contentType), item)
-      createNode(node)
-    })
-  })
-}
+  return function (_x, _x2) {
+    return _ref.apply(this, arguments);
+  };
+}();
